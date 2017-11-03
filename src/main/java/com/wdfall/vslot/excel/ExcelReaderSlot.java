9eefaae9383780e479e05c92837c60764ac02aa2
windfall <windfall@DESKTOP-6OKI018>
2017-11-03 15:54:56 +0900

    excel reading

--- src/main/java/com/wdfall/vslot/excel/ExcelReaderSlot.java
+++ src/main/java/com/wdfall/vslot/excel/ExcelReaderSlot.java
@@ -18,6 +18,173 @@ public class ExcelReaderSlot extends ExcelReader {
 
 	private SlotGameSettingParam slotGameSettingParam;
 	
+	public ExcelReaderSlot() {
+		
+	}
+	
+	@Override
+	protected void parseData(List<List<String>> excelData) {
+		log.debug("parseData !!");
+		slotGameSettingParam = new SlotGameSettingParam();
+		
+		//1.  {{reel-define}}
+		// 1.1 find start - end
+		parseDataItemReelDefine(excelData);
+		
+		//2.  {{dumm-define}}
+		parseDataItemDummDefine(excelData);
+		
+	}
+
+	private void parseDataItemDummDefine(List<List<String>> excelData) {
+		
+		// 1.1 loc
+		ParamItemLocation loc = findParamItem(excelData, "dumm-define");
+		
+		// 1.2 dataLoc
+		ParamItemLocation dataLoc = loc.getDataLocationDefault();
+		log.debug("dataLoc = {}", dataLoc);
+		
+		// 3. 릴1, 릴2 ...
+		int reel1Index = dataLoc.getCellIndexStart();
+		List<String> reelList1 = readExcelDataVertical(excelData, dataLoc, reel1Index);
+		
+		int reel2Index = dataLoc.getCellIndexStart() + 1;
+		List<String> reelList2 = readExcelDataVertical(excelData, dataLoc, reel2Index);
+		
+		log.info("reelList1 = {}", reelList1);
+		log.info("reelList2 = {}", reelList2);
+	}
+
+	private void parseDataItemReelDefine(List<List<String>> excelData) {
+		List<Map<String, Integer>> reelCompositionParamList = new ArrayList<>();
+		
+		// 1.1 loc
+		ParamItemLocation loc = findParamItem(excelData, "reel-define");
+		
+		// 1.2 trimedLoc: 심볼,합계 remove
+		ParamItemLocation trimedLoc = loc.copy();
+		trimedLoc.setCellIndexStart(loc.getCellIndexStart() + 1);
+		trimedLoc.setRowIndexEnd(loc.getRowIndexEnd() - 1);
+
+		// 1.3 dataLoc
+		ParamItemLocation dataLoc = trimedLoc.getDataLocationDefault();
+		log.debug("dataLoc = {}", dataLoc);
+		
+		// 2. sysmbolId list
+		int ciID = dataLoc.getCellIndexStart();
+		List<String> symbolIdList = readExcelDataVertical(excelData, dataLoc, ciID);
+		
+		// 3. 릴1, 릴2 ...
+		for(int ciReel=dataLoc.getCellIndexStart() + 1; ciReel<dataLoc.getCellIndexEnd(); ciReel++) {
+			List<String> reelList = readExcelDataVertical(excelData, dataLoc, ciReel);
+			
+			Map<String, Integer> reelComp = generateReelCompositionMap(symbolIdList, reelList);
+			reelCompositionParamList.add(reelComp);
+		}
+		
+		log.info("{}", reelCompositionParamList); 
+		
+		slotGameSettingParam.setReelCompositionParamList(reelCompositionParamList );
+	}
+
+	
+	
+	private Map<String, Integer> generateReelCompositionMap(List<String> symbolIdList, List<String> reel1List) {
+		Map<String, Integer> reelComp = new HashMap<>();
+		for(int i=0; i<symbolIdList.size(); i++) {
+			String symbolID = symbolIdList.get(i);
+			String countString = reel1List.get(i);
+			Integer count = (int)(double)Double.valueOf(countString);
+			reelComp.put(symbolID, count);
+		}
+		return reelComp;
+	}
+	
+	private List<String> readExcelDataVertical(List<List<String>> excelData, ParamItemLocation dataLoc, int cellIndex) {
+		int rowIndexStart = dataLoc.getRowIndexStart();
+		int rowIndexEnd = dataLoc.getRowIndexEnd();
+		List<String> dataList = new ArrayList<>();
+		for(int i=rowIndexStart; i<rowIndexEnd; i++) {
+			String data = excelData.get(i).get(cellIndex);
+			dataList.add(data);
+		}
+		return dataList;
+	}
+	
+	private List<String> readExcelDataHorizontal(List<List<String>> excelData, ParamItemLocation dataLoc, int rowIndex) {
+		int cellIndexStart = dataLoc.getCellIndexStart();
+		int cellIndexEnd = dataLoc.getCellIndexEnd();
+		List<String> dataList = new ArrayList<>();
+		for(int i=cellIndexStart; i<cellIndexEnd; i++) {
+			String data = excelData.get(rowIndex).get(i);
+			dataList.add(data);
+		}
+		return dataList;
+	}
+
+	/**
+	 * <<header포함 데이터 영역 조회>>
+	 * 
+	 * header영역은 첫번째 row
+	 * start포함, end 배제 [row start,end),[cell start,end) 
+	 * row start는 {{inputSymbol}} next row 부터
+	 * row end는 {{inputSymbol}} 까지
+	 * cell start는 {{inputSymbol}}이 있는 cell 부터
+	 * cell end는 header영역에서 첫번째 null인 cell 까지 
+	 * @param excelData
+	 * @param inputSymbol
+	 * @return
+	 */
+	private ParamItemLocation findParamItem(List<List<String>> excelData, String inputSymbol) {
+		ParamItemLocation result = new ParamItemLocation();
+		String paramReelDefineStart = String.format("{{%s}}", inputSymbol);
+		String paramReelDefineEnd = String.format("{{//%s}}", inputSymbol);
+		for(int rowIndex=0; rowIndex<excelData.size(); rowIndex++) {
+			List<String> rowData = excelData.get(rowIndex);
+			
+			for(int cellIndex=0; cellIndex<rowData.size(); cellIndex++) {
+				String cellData = rowData.get(cellIndex);
+				
+				// 
+				if(paramReelDefineStart.equalsIgnoreCase(cellData)) {
+					result.setRowIndexStart(rowIndex + 1);
+					result.setCellIndexStart(cellIndex);
+				}
+				
+				//
+				if(paramReelDefineEnd.equalsIgnoreCase(cellData)) {
+					result.setRowIndexEnd(rowIndex);
+					break;
+				}
+				
+			}
+		}
+		
+		//
+		findParamItemSetCellIndexEnd(excelData, result);
+		
+		return result;
+	}
+
+	private void findParamItemSetCellIndexEnd(List<List<String>> excelData, ParamItemLocation result) {
+		List<String> headerList = excelData.get(result.getRowIndexStart());
+		int lastCellOffset = headerList.size();
+		for(int i=0; i<headerList.size(); i++) {
+			String item = headerList.get(i);
+			if(item == null) {
+				lastCellOffset = i;
+				break;
+			}
+		}
+		int lastCellIndex = lastCellOffset + result.getCellIndexStart();
+		result.setCellIndexEnd(lastCellIndex);
+	}
+
+	public SlotGameSettingParam getSlotGameSettingParam() {
+		return slotGameSettingParam;
+	}
+	
 	@Data
 	private static class ParamItemLocation {
 		//include start , not include end 
@@ -30,126 +197,22 @@ public class ExcelReaderSlot extends ExcelReader {
 		public ParamItemLocation copy() {
 			return CloneUtils.deepCopyByLib(this);
 		}
-	}
-	
-	public ExcelReaderSlot() {
 		
-	}
-	
-	@Override
-	protected void parseData(List<List<String>> excelData) {
-		log.info("parseData !!");
-		slotGameSettingParam = new SlotGameSettingParam();
-		
-		//1.  {{reel-define}}
-		// 1.1 find start - end
-		parseDataItemReelDefine(excelData);
-		
-		//2.  {{dumm-define}}
-		//......
-		
-	}
-
-	private void parseDataItemReelDefine(List<List<String>> excelData) {
-		List<Map<String, Integer>> reelCompositionParamList = new ArrayList<>();
-		
-		ParamItemLocation loc = findParamItem(excelData, "reel-define");
-		
-		log.info("loc = {}", loc);
-		// 심볼 remove
-		// 합계 remove
-		ParamItemLocation realLoc = loc.copy();
-		realLoc.setCellIndexStart(loc.getCellIndexStart() + 1);
-		realLoc.setRowIndexEnd(loc.getRowIndexEnd() - 1);
-
-		
-		int rowDataStart = realLoc.getRowIndexStart() + 1;
-		int rowDataEnd = realLoc.getRowIndexEnd();
-		
-		// 1. sysmbolId list
-		int ciID = realLoc.getCellIndexStart();
-		List<String> symbolIdList = readExcelDataVertical(excelData, ciID, rowDataStart, rowDataEnd);
-		
-		// 릴1, 릴2
-		for(int ciReel=realLoc.getCellIndexStart() + 1; ciReel<realLoc.getCellIndexEnd(); ciReel++) {
-			List<String> reel1List = readExcelDataVertical(excelData, ciReel, rowDataStart, rowDataEnd);
-			
-			Map<String, Integer> reelComp = new HashMap<>();
-			for(int i=0; i<symbolIdList.size(); i++) {
-				String symbolID = symbolIdList.get(i);
-				String countString = reel1List.get(i);
-				Integer count = (int)(double)Double.valueOf(countString);
-				reelComp.put(symbolID, count);
-			}
-			reelCompositionParamList.add(reelComp);
+		/**
+		 * data location default
+		 * 1) 첫번째행은 헤더
+		 * 2) 나머지는 data
+		 * @return
+		 */
+		// data location 
+		// 1) 첫번째행은 헤더
+		// 2) 나머지는 data
+		public ParamItemLocation getDataLocationDefault() {
+			ParamItemLocation dataLoc = CloneUtils.deepCopyByLib(this);
+			dataLoc.setRowIndexStart(dataLoc.getRowIndexStart() + 1);
+			return dataLoc;
 		}
 		
-		slotGameSettingParam.setReelCompositionParamList(reelCompositionParamList );
-	}
-	
-	private List<String> readExcelDataVertical(List<List<String>> excelData, int cellIndex, int rowIndexStart, int rowIndexEnd) {
-		List<String> dataList = new ArrayList<>();
-		for(int i=rowIndexStart; i<rowIndexEnd; i++) {
-			String data = excelData.get(i).get(cellIndex);
-			dataList.add(data);
-		}
-		return dataList;
-	}
-	
-	private List<String> readExcelDataHorizontal(List<List<String>> excelData, int rowIndex, int cellIndexStart, int cellIndexEnd) {
-		List<String> dataList = new ArrayList<>();
-		for(int i=cellIndexStart; i<cellIndexEnd; i++) {
-			String data = excelData.get(rowIndex).get(i);
-			dataList.add(data);
-		}
-		return dataList;
-	}
-
-	private ParamItemLocation findParamItem(List<List<String>> excelData, String paramString) {
-		ParamItemLocation result = new ParamItemLocation();
-		String paramReelDefineStart = String.format("{{%s}}", paramString);
-		String paramReelDefineEnd = String.format("{{//%s}}", paramString);
-		for(int rowIndex=0; rowIndex<excelData.size(); rowIndex++) {
-			List<String> rowData = excelData.get(rowIndex);
-			
-			for(int cellIndex=0; cellIndex<rowData.size(); cellIndex++) {
-				String cellData = rowData.get(cellIndex);
-				
-				if(paramReelDefineStart.equalsIgnoreCase(cellData)) {
-					result.setRowIndexStart(rowIndex + 1);
-					result.setCellIndexStart(cellIndex);
-				}
-				
-				findParamItemSetCellIndexEnd(result, rowIndex, rowData, cellIndex, cellData);
-				
-				if(paramReelDefineEnd.equalsIgnoreCase(cellData)) {
-					result.setRowIndexEnd(rowIndex);
-					break;
-				}
-				
-			}
-		}
-		return result;
-	}
-
-	private void findParamItemSetCellIndexEnd(ParamItemLocation result, int rowIndex, List<String> rowData,
-			int cellIndex, String cellData) {
-		//find data header row last cell index 
-		int itemDataRow = result.getRowIndexStart();
-		if(itemDataRow != -1 && rowIndex == itemDataRow) {
-			log.info("({}, {}), cellData = {}", rowIndex, cellIndex, cellData);
-			
-			if(result.getCellIndexEnd() != -1) { // if not find cellIndexEnd
-				boolean isFirstNonNullCell = (cellData == null) || (cellIndex == rowData.size() - 1);
-				if(isFirstNonNullCell ) {
-					result.setCellIndexEnd(cellIndex + 1);
-				}
-			}
-		}
-	}
-	
-	public SlotGameSettingParam getSlotGameSettingParam() {
-		return slotGameSettingParam;
 	}
 	
 	public static void main(String[] args) throws Exception { 
