d1b67b5ba04d94dc20ec7e02129ac91022e7c849
mr838wind <mr838wind@gmail.com>
2017-11-10 14:18:53 +0900

    test

--- src/test/java/com/wdfall/vslot/SlotSimulatorTestStage1.java
+++ src/test/java/com/wdfall/vslot/SlotSimulatorTestStage1.java
@@ -1,18 +1,17 @@
 package com.wdfall.vslot;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 
-import com.wdfall.vslot.game.SlotGameRegular;
+import com.wdfall.vslot.game.SlotGameStage1;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SlotSimulatorTestStage1 {
 	
-	private static final String JSON_FILE_PATH = "slot_game_setting_param_stage1.json";
+private static final String EXCEL_FILE_PATH = "vslot_input_main.xlsx";
 	
 	/**
 	 * log4j level ---> INFO
@@ -24,16 +23,12 @@ public class SlotSimulatorTestStage1 {
 	}
 
 	@Test
-	public void testIntegrationBasic() throws Exception {
-		commonTest(JSON_FILE_PATH, 145.0805, 2);
-	}
-
-	private void commonTest(String jsonFilePath, double payoutExpected, double maxDiff) throws Exception {
-		SlotSimulator slotSimulator = new SlotSimulator(jsonFilePath);
-		slotSimulator.setPayoutExpected(payoutExpected);
-		slotSimulator.startWithThread(SlotGameRegular.class);
+	public void testStage1() throws Exception {
+		SlotSimulator slotSimulator = new SlotSimulator(EXCEL_FILE_PATH, "slot_input_stage1");
+		//slotSimulator.setPayoutExpected(payoutExpected);
+		slotSimulator.startWithThread(SlotGameStage1.class);
 		
-		Assert.assertTrue( slotSimulator.getDifference() < maxDiff );
+		//Assert.assertTrue( slotSimulator.getDifference() < maxDiff );
 	}
 	
 	
